79c59fb6dabbafb8cbe18c264979209124ad5f70
gary <everydaylinuxuser@gmail.com>
2025-05-25 12:54:03 +0100

    added debian scripts folder
    added debian scripts folder and script to resize KVM virtual machine screen resolutions

---
+++ debian/debianvmscripts/screenresolution.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this will enable you to amend the screen resolution in a KVM virtual machine
+# after running this script reboot the VM
+sudo apt install spice-vdagent spice-webdavd
